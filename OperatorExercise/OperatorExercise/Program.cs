916d91547d18d4cb285c88b00440c28fb71fc9e4
Philip Goforth <77207179+DrDiscoChickenShack@users.noreply.github.com>
2021-10-22 13:28:59 -0500

    added while loop

--- OperatorExercise/OperatorExercise/Program.cs
+++ OperatorExercise/OperatorExercise/Program.cs
@@ -13,39 +13,43 @@ namespace OperatorExercise
         }
         public static string basicMath(int a, int b)
         {
-
             int sum = a + b;
             int quotient = a / b;
             int sub = a - b;
             int mult = a * b;
             int remainder = a % b;
-
-           
             
-            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
-            
-           
-            return "Thank You\n";
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}\nThe sum is {sum}.\n {a} x {b} is {mult} and {a} - {b} is {sub}.");
+            return "\nArea of Circle";
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a large number:");
-            int x = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter a smaller number:");
-            int y = int.Parse(Console.ReadLine());
-            Console.WriteLine(operatorTest.basicMath(x, y));
-           
+            bool runAgain = true;
+            while (runAgain)
+            {
+                Console.WriteLine("Enter a large number:");
+                int x = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter a smaller number:");
+                int y = int.Parse(Console.ReadLine());
+                Console.WriteLine(operatorTest.basicMath(x, y));
 
-            Console.WriteLine("Enter radius of circle:\n");
-            Console.WriteLine($"The area of the circle is {operatorTest.AreaOfCircle(double.Parse(Console.ReadLine()))}");
-            Console.ReadLine();
-            
-            
+                Console.WriteLine("Enter radius of circle:");
+                Console.WriteLine($"The area of the circle is {operatorTest.AreaOfCircle(double.Parse(Console.ReadLine()))}");
 
-
+                Console.WriteLine("Run again? (y/n)");
+                string z = Console.ReadLine();
+                if (z == "y")
+                {
+                    runAgain = true;
+                }
+                else
+                {
+                    runAgain = false;
+                }
+            }
         }
     }
 }
